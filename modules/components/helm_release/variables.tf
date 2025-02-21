5047390f8a570433a72d3b32a07f9ce041730d63
gildani007 <122787127+gildani007@users.noreply.github.com>
2025-02-22 00:28:45 +0200

    Update variables.tf with default

--- modules/components/helm_release/variables.tf
+++ modules/components/helm_release/variables.tf
@@ -1,20 +1,22 @@
 variable "release_name" {
   type        = string
   description = "The Helm release name"
+  default     = "unset"
 }
 
 variable "namespace" {
   type        = string
   description = "The namespace for the Helm release"
+  default     = "unset"
 }
 
 variable "chart" {
   type        = string
   description = "The Helm chart path"
+  default     = "unset"
 }
 
 variable "values" {
   type        = any
   description = "Helm values as a map"
 }
-
